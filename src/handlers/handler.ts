5b3f029bf9b909ca70268691b1a683effd06bf2a
paragmore <paragmore306@gmail.com>
2024-07-11 18:27:28 +0530

    Basic service boilerplate

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,17 +1,36 @@
+import async from 'async'
+
 import HandlerBase from 'sarafa-svc-node-handler-base'
 import { CmdOptions } from 'sarafa-svc-node-wrk'
 
 class HttpHandler extends HandlerBase {
+  constructor (conf: any, cmdOptions: CmdOptions) {
+    super({}, cmdOptions as any)
+    this.init()
+    this.start(() => console.log('START'))
+  }
 
-    constructor(conf: any, cmdOptions: CmdOptions) {
-        super({}, cmdOptions as any)
-        console.log("C", cmdOptions)
-        this.start(() => console.log("START"))
-    }
+  _init () {
+    this.servicesArgs = [
+      {
+        library: 'sarafa-svc-fastify',
+        name: 'sarafaSvcFastify',
+        opts: {
+          logger: true,
+          trustProxy: true,
+          disableRequestLogging: true,
+          port: 3649
+        },
+        delayStart: true
+      }
+    ]
+  }
 
-    _start(cb: (err?: Error | null, results?: any) => void) {
-        console.log("PROSS start")
-    }
+  _start (cb: (err?: Error | null, results?: any) => void) {
+    this.services.sarafaSvcFastify.start()
+  }
+
+  _stop (cb: (err?: Error | null, results?: any) => void): void {}
 }
 
 export default HttpHandler
